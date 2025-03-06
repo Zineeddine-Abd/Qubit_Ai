5390b37c116e3b9311f03a0c4cb3bd94835e7d40
Abdeladim Zine eddine <zineeddine.abdeladim@gmail.com>
2025-03-06 23:40:33 +0100

    fixed the jwt secret loading and changed its value

--- src/Server/server.js
+++ src/Server/server.js
@@ -11,9 +11,7 @@ app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 // Use secret from environment variables
-const SECRET_KEY = process.env.JWT_SECRET || 'e1a3fd21c749535dc052bcc442d3e5ea4cf0763278fb69534014eec1ccb15d';
-console.log("JWT_SECRET:", SECRET_KEY);
-
+const SECRET_KEY = process.env.JWT_SECRET;
 
 // MongoDB connection
 mongoose.connect("mongodb://localhost:27017/qubit", {
