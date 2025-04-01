4c28296c543adced42d98efcb33f1e7b67be7158
Abdeladim Zine eddine <zineeddine.abdeladim@gmail.com>
2025-04-01 13:25:59 +0100

    added frontend baseurl to backend env

--- Server/server.js
+++ Server/server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_BASEURL, credentials: true }));
 
 // Use secret from environment variables
 const SECRET_KEY = process.env.JWT_SECRET;
